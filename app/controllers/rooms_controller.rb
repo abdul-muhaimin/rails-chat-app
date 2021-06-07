e681992ee6e91327582ad50fa4d3e7309c06caf2
abdul muhaimin <muhai07@outlook.com>
2021-06-07 22:36:34 +0900

    form for creating new room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,48 @@
 class RoomsController < ApplicationController
+  # Loads:
+  # @rooms = all rooms
+  # @room = current room when applicable
+  before_action :set_room
+
   def index
+    @rooms = Room.all
   end
 
+  def new
+    @room = Room.new
+  end
 
+  def create
+    @room = Room.new room_param
 
+    if @room.save
+      flash[:success] = "Room #{@room.name} was created successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update_attributes(room_param)
+      flash[:success] = "Room #{@room.name} was updated successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_room
+    @rooms = Room.all
+    @room = Room.find(params[:id]) if params[:id]
+  end
+
+  def room_param
+    params.require(:room).permit(:name)
+  end
 end
