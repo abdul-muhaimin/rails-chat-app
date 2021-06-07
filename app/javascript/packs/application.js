4306955bff8eda44a9d67656bdae72b07a5d1bcc
abdul muhaimin <muhai07@outlook.com>
2021-06-07 23:00:08 +0900

    added code for send messages in to rooms + CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import "room"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
