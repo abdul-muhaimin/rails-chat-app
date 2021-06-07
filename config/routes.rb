d7442288ab7ab1bffb643d9f32f5e2073e8a9301
abdul muhaimin <muhai07@outlook.com>
2021-06-07 21:48:29 +0900

    added sign up form for new users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :room_messages
   resources :rooms
   devise_for :users
-  root to: 'pages#home'
+  root to: 'rooms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
